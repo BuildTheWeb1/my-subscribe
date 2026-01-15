894a23b04b07626b801f14cc81878e025c1dd627
ClaudiuC <build.the.web1@gmail.com>
2026-01-15 12:40:49 +0200

    Initial Commit

---
+++ MySubscribe/MySubscribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySubscribeApp.swift
+//  MySubscribe
+//
+//  Created by WebSpace Developer on 15.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MySubscribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MySubscribeTests/MySubscribeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MySubscribeTests.swift
+//  MySubscribeTests
+//
+//  Created by WebSpace Developer on 15.01.2026.
+//
+
+import Testing
+@testable import MySubscribe
+
+struct MySubscribeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
